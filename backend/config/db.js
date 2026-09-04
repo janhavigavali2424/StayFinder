c1dfe3a04dbe5d9db00f0ceb6a9ec1079d7ebc42
janhavigavali2424 <janhavigavali44@gmail.com>
2026-09-04 12:38:42 -0700

    Configure Aiven MySQL SSL connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,21 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
-
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'stayfinder_db',
     port: process.env.DB_PORT || 3306,
+
+    ssl: {
+        rejectUnauthorized: false
+    },
+
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    connectTimeout: 2000
+    connectTimeout: 10000
 });
-
 let isMysqlConnected = false;
 let dbReady = false;
 
